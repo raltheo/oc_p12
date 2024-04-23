47af538f7cb427f1c507c8178c739178b683fd11
Théo <cobaltium360@gmail.com>
2024-04-23 14:55:03 +0200

    ajout des fonctionnalité contrat, je suis malade c'est assez pénible. J'espere que samedi je serai plus malade

--- app/controllers/contrat.py
+++ app/controllers/contrat.py
@@ -1,7 +1,7 @@
 from pydoc import cli
 from app.middleware import login_require, require_role
 from app.models import Contrat, Client
-from app.views import menu_contrat_view, create_contrat_view, show_contrat
+from app.views import menu_contrat_view, create_contrat_view, show_contrat, delete_contrat_view, update_contrat_view
 from app.controllers.client import liste_client
 from app.utils import red_print, green_print
 
@@ -27,7 +27,6 @@ def create_contrat(session, client_id, montant_total, montant_restant, status_co
     client = session.query(Client).filter_by(clientId=client_id).first()
     if client:
         
-        
         contrat = Contrat(
             clientId=client.clientId,
             commercialId=client.collaborateurId,
@@ -43,8 +42,40 @@ def create_contrat(session, client_id, montant_total, montant_restant, status_co
             session.rollback()
             return False, "Une erreur est survenue."
 
-
     return False, "Client introuvable."
+
+@login_require
+@require_role(["admin", "gestion"])
+def delete_contrat(session, contrat_id, collaborateur_id=None, user_role=None):
+    contrat = session.query(Contrat).filter_by(contratId=contrat_id).first()
+    if contrat:
+            session.delete(contrat)
+            session.commit()
+            return True, "Contrat supprimé."
+    return False, "Contrat introuvable"
+
+@login_require
+@require_role(["admin", "gestion", "commercial"])
+def update_contrat(session, contrat_id, col, data, collaborateur_id=None, user_role=None):
+    contrat = session.query(Contrat).filter_by(contratId=contrat_id).first()
+    if contrat.client.collaborateurId != collaborateur_id and user_role == "commercial":
+        return False, "Vous ne pouvez pas mettre a jour les contrat d'autres clients"
+    if contrat:
+        try:
+            if col == 1: contrat.montant_total = data
+            elif col == 2: contrat.montant_restant = data
+            elif col == 3: 
+                if data in ["unsigned", "signed"]:
+                    contrat.status_contrat = data
+            else: return False, "Mauvais choix"
+            session.commit()
+            return True, "Contrat mis a jour !"
+        except Exception as e:
+            print(e)
+            session.rollback()
+            return False, "Une erreur est survenue !"
+    return False, "Contrat introuvable"
+
 
 @login_require
 def menu_contrat(session, collaborateur_id=None, user_role=None):
@@ -55,6 +86,20 @@ def menu_contrat(session, collaborateur_id=None, user_role=None):
         if choix == 2:
             client_id, montant_total, montant_restant, status_contrat = create_contrat_view(liste_client(session))
             state, message = create_contrat(session, client_id, montant_total, montant_restant, status_contrat)
+            if state == True:
+                green_print(message)
+            else:
+                red_print(message)
+        if choix == 3:
+            id_contrat, col, new_data = update_contrat_view(liste_contrat(session))
+            state, message = update_contrat(session, id_contrat, col, new_data)
+            if state == True:
+                green_print(message)
+            else:
+                red_print(message)
+        if choix == 4:
+            contrat_id = delete_contrat_view(liste_contrat(session))
+            state, message = delete_contrat(session, contrat_id)
             if state == True:
                 green_print(message)
             else:
--- app/views/contrat.py
+++ app/views/contrat.py
@@ -1,3 +1,4 @@
+
 from app.views import show_client
 from prettytable import PrettyTable
 
@@ -17,7 +18,7 @@ def show_contrat(contrats):
     print("\n")
 
 def menu_contrat_view():
-    print("Gestion des Clients")
+    print("Gestion des Contrats")
     print("    1: Afficher les Contrats")
     print("    2: Crée un contrat")
     print("    3: Modifier un contrat")
@@ -31,5 +32,30 @@ def create_contrat_view(clients):
     client_id = input("Choisissez l'ID du client pour le contrat : ")
     montant_total = input("Entrez le montant total : ")
     montant_restant = input("Entrez le montant restant : ")
-    status_contrat = input("Entrez le status du contrat (unsigned ou signed) : ")
+    while True:
+        status_contrat = input("Entrez le status du contrat (unsigned ou signed) : ")
+        if status_contrat == "signed" or status_contrat == "unsigned":
+            break
     return client_id, montant_total, montant_restant, status_contrat
+
+def delete_contrat_view(contrats):
+    show_contrat(contrats)
+    contrat_id = int(input("Entrez l'Id du contrat a supprimer : "))
+    return contrat_id
+
+def update_contrat_view(contrats):
+    show_contrat(contrats)
+    col_to_update = {"1": "montant total", "2": "montant restant", "3": "status du contrat"}
+    id_contrat = input("Choisissez l'ID du contrat à modifié : ")
+    [print(f"    {key}: {value}") for key, value in col_to_update.items()]
+    choix = input("Choisissez un champ a modifier : ")
+    try : 
+        col_to_update[choix]
+        col = int(choix)
+    except:
+        print("merci de faire correctement")
+    while True:
+        new_data = input("Entrez la valeur (signed ou unsigned): ")
+        if new_data == "signed" or new_data == "unsigned":
+            break
+    return id_contrat, col, new_data
